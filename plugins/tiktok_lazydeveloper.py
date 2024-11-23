b098f27161b3fca5db7039b8c08cffe5c117d9c8
lazysuperstar <itz.me.neha.roy@gmail.com>
2024-11-24 01:29:51 +0530

    Update tiktok_lazydeveloper.py

--- plugins/tiktok_lazydeveloper.py
+++ plugins/tiktok_lazydeveloper.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import requests
 import time
 
+
 from config import TEL_USERNAME
 @Client.on_message(filters.private & filters.text & ~filters.command(['start', 'help']))
 async def handle_tiktok_download(client: Client, message: Message):
@@ -22,7 +23,7 @@ async def handle_tiktok_download(client: Client, message: Message):
         # Download video using tiktok_downloader (snaptik)
         try:
             res = snaptik(url)
-            video_url = res[0].download()  # Get the download URL directly without saving to disk
+            # video_url = res[0].download()  # Get the download URL directly without saving to disk
             # lazydevelopercaption = res[0].caption  # Get the caption of the video
         except Exception as e:
             await message.reply(f"❌ Failed to download the video: {e}")
@@ -30,6 +31,24 @@ async def handle_tiktok_download(client: Client, message: Message):
         
         bot_username = client.username if client.username else TEL_USERNAME
         caption_lazy = f".\nᴡɪᴛʜ ❤ @{bot_username}\n."
+        path = f"{message.chat.id}_{time.time()}_downloadedby_@{client.username}.mp4" 
+
+        if res:
+            try:
+                print(res)
+                video_url = res[0].download(path)
+                print(f"Video downloaded :  {video_url}")
+                # input_file = types.InputFile(f"{message.message_id}.mp4")
+                # file_id = client.upload.saveFilePart(file_path)
+
+                await client.send_video(message.chat.id, video_url, caption=caption_lazy)
+            except Exception as lazyerror:
+                print(lazyerror)
+            # xlaxyx = await client.send_document(
+            #     chat_id=message.chat.id,
+            #     document=input_file,
+            #     caption=caption_lazy  # Add the caption of the TikTok video
+            # )
 
         # lazy_caption = lazydevelopercaption
         # while len(lazy_caption) + len(caption_lazy) > 1024:
@@ -37,26 +56,26 @@ async def handle_tiktok_download(client: Client, message: Message):
         # lazy_caption = lazy_caption + caption_lazy
         
         # Fetch the video content as a stream
-        response = requests.get(video_url, stream=True)
-        if response.status_code != 200:
-            await message.reply("❌ Failed to fetch the video.")
-            return
+        # response = requests.get(video_url, stream=True)
+        # if response.status_code != 200:
+        #     await message.reply("❌ Failed to fetch the video.")
+        #     return
         
         # Create an in-memory file using BytesIO
-        video_bytes = BytesIO(response.content)
-        video_bytes.name = f"{message.chat.id}_{time.time()}_downloadedby_@{client.username}.mp4" 
+        # video_bytes = BytesIO(response.content)
+        # video_bytes.name = f"{message.chat.id}_{time.time()}_downloadedby_@{client.username}.mp4" 
         
         # Send the video and caption directly from memory
-        input_file = types.InputMediaDocument(media=video_bytes, caption=caption_lazy)
-        await msg_del.delete()
+        # input_file = types.InputMediaDocument(media=video_bytes, caption=caption_lazy)
+        # await msg_del.delete()
         
         # If there is a caption, send it with the video
        
-        xlaxyx = await client.send_document(
-                chat_id=message.chat.id,
-                document=input_file,
-                caption=caption_lazy  # Add the caption of the TikTok video
-            )
+        # xlaxyx = await client.send_document(
+        #         chat_id=message.chat.id,
+        #         document=input_file,
+        #         caption=caption_lazy  # Add the caption of the TikTok video
+        #     )
         
         
     except Exception as e:
